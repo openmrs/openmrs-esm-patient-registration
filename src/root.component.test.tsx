f9afaefb795dce1bb596bb9059c6fe614ee00ebb
Nicholas J Hill <nick.hill@thoughtworks.com>
2020-05-14 10:10:48 +0100

    NH LK - NC - Tests for root component

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { cleanup, render } from '@testing-library/react';
 import Root from './root.component';
-
-afterAll(cleanup);
 
 window['getOpenmrsSpaBase'] = jest.fn().mockImplementation(() => '/');
 
-describe(`<Root />`, () => {
-  afterEach(cleanup);
-  it(`renders without dying`, () => {
+afterAll(cleanup);
+
+describe('root component', () => {
+  it('renders without dying', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Root />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the main (parent) component', () => {
     const wrapper = render(<Root />);
+    expect(wrapper.container.getElementsByTagName('main')).toHaveLength(1);
+    expect(wrapper.container.getElementsByClassName('omrs-main-content')).toHaveLength(1);
   });
 });
