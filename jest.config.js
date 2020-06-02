9ec04ebbf794ff436c04ab7c2b973f7a1d497475
Nicholas J Hill <nick.hill@thoughtworks.com>
2020-06-02 14:44:08 +0100

    NH DK - MF-191 - Remove unused modules and mocking new ones

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '\\.(css)$': 'identity-obj-proxy',
     '@openmrs/esm-api': '<rootDir>/__mocks__/openmrs-esm-api.mock.tsx',
     '@openmrs/esm-module-config': '<rootDir>/__mocks__/openmrs-esm-module-config.mock.tsx',
+    '@openmrs/esm-error-handling': '<rootDir>/__mocks__/openmrs-esm-error-handling.mock.tsx',
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupFilesAfterEnv: ['<rootDir>/src/setup-enzyme.ts'],
