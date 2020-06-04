a474f6f794dedacddd42f8bb4fa5489279e811da
Nicholas J Hill <nick.hill@thoughtworks.com>
2020-06-04 20:03:12 +0100

    NH - MF-194/5 - Estimated age and date of birth functionality

--- src/patient-registration/section/personal-details.component.tsx
+++ src/patient-registration/section/personal-details.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import dayjs from 'dayjs';
 import styles from './personal-details.css';
 
@@ -17,7 +17,6 @@ export interface PersonalDetailsState {
   gender: string;
   birthDate: Date;
   birthTime: Date;
-  age: { years: number; months: number; days: number };
   estimate: boolean;
 }
 
@@ -33,8 +32,12 @@ export function PersonalDetails(props: PersonalDetailsProps) {
     gender: '',
     birthDate: null,
     birthTime: null,
-    age: { years: 0, months: 0, days: 0 },
     estimate: false,
+  });
+
+  const [age, setAge] = useState<{ years: number; months: number }>({
+    years: 0,
+    months: 0,
   });
 
   useEffect(() => {
@@ -46,6 +49,26 @@ export function PersonalDetails(props: PersonalDetailsProps) {
       setNames('');
     }
   }, [personalDetails.nameUnknown]);
+
+  useEffect(() => {
+    if (age.years !== 0 || age.months !== 0) {
+      setPersonalDetails({
+        ...personalDetails,
+        birthDate: dayjs()
+          .subtract(age.years, 'year')
+          .subtract(age.months, 'month')
+          .toDate(),
+      });
+    }
+  }, [age]);
+
+  useEffect(() => {
+    if (personalDetails.estimate) {
+      setPersonalDetails({ ...personalDetails, birthDate: null });
+    } else {
+      setAge({ years: 0, months: 0 });
+    }
+  }, [personalDetails.estimate]);
 
   const setNames = (name: string) => {
     setPersonalDetails({
@@ -59,204 +82,178 @@ export function PersonalDetails(props: PersonalDetailsProps) {
     });
   };
 
+  const getAgeInputs = () => {
+    return (
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Age</h1>
+        <section className={styles.column}>
+          <label className={styles.fieldLabel} htmlFor="years">
+            Years
+          </label>
+          <input
+            type="number"
+            id="years"
+            name="years"
+            value={age.years}
+            className={styles.fieldInput}
+            onChange={event => setAge({ ...age, years: event.target.valueAsNumber })}
+          />
+        </section>
+        <section className={styles.column}>
+          <label className={styles.fieldLabel} htmlFor="months">
+            Months
+          </label>
+          <input
+            type="number"
+            id="months"
+            name="months"
+            value={age.months}
+            className={styles.fieldInput}
+            onChange={event => setAge({ ...age, months: event.target.valueAsNumber })}
+          />
+        </section>
+      </section>
+    );
+  };
+
   return (
     <main className={styles.container}>
-      <section className={styles.headers}>
-        <div className={styles.header}>
-          <h1 className={styles.fieldHeader}>Name</h1>
-        </div>
-        <div className={styles.header}>
-          <h1 className={styles.fieldHeader}>Additional Name</h1>
-        </div>
-        <div className={styles.header}>
-          <h1 className={styles.fieldHeader}>Gender</h1>
-        </div>
-        <div className={styles.header}>
-          <h1 className={styles.fieldHeader}>Age</h1>
-        </div>
-        <div className={styles.header}>
-          <h1 className={styles.fieldHeader}>Birth Date</h1>
-        </div>
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Name</h1>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Given Name"
+            value={personalDetails.givenName}
+            name="given-name"
+            className={styles.fieldInput}
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, givenName: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Middle Name"
+            value={personalDetails.middleName}
+            name="middle-name"
+            className={styles.fieldInput}
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, middleName: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Family Name"
+            value={personalDetails.familyName}
+            name="family-name"
+            className={styles.fieldInput}
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, familyName: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Name Unknown</h1>
+          <input
+            type="checkbox"
+            id="name-unknown"
+            name="name-unknown"
+            className={styles.fieldInput}
+            checked={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, nameUnknown: event.target.checked })}
+          />
+        </section>
       </section>
-      <section className={styles.inputs}>
-        <div className={styles.input}>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Given Name"
-              value={personalDetails.givenName}
-              name="given-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, givenName: event.target.value })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Middle Name"
-              value={personalDetails.middleName}
-              name="middle-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, middleName: event.target.value })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Family Name"
-              value={personalDetails.familyName}
-              name="family-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, familyName: event.target.value })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <label htmlFor="name-unknown">Name Unknown</label>
-            <input
-              type="checkbox"
-              id="name-unknown"
-              name="name-unknown"
-              checked={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, nameUnknown: event.target.checked })}
-            />
-          </div>
-        </div>
-        <div className={styles.input}>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Additional Given Name"
-              value={personalDetails.additionalGivenName}
-              name="additional-given-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, additionalGivenName: event.target.value })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Additional Middle Name"
-              value={personalDetails.additionalMiddleName}
-              name="additional-middle-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, additionalMiddleName: event.target.value })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <input
-              type="text"
-              placeholder="Additional Family Name"
-              value={personalDetails.additionalFamilyName}
-              name="additional-family-name"
-              disabled={personalDetails.nameUnknown}
-              onChange={event => setPersonalDetails({ ...personalDetails, additionalFamilyName: event.target.value })}
-            />
-          </div>
-        </div>
-        <div className={styles.input}>
-          <select
-            name="gender"
-            value={personalDetails.gender}
-            onChange={event => setPersonalDetails({ ...personalDetails, gender: event.target.value })}>
-            <option value="" disabled>
-              Select
-            </option>
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-            <option value="O">Other</option>
-            <option value="U">Unknown</option>
-          </select>
-        </div>
-        <div className={styles.input}>
-          <div className={styles.subInput}>
-            <label className={styles.fieldSubLabel} htmlFor="years">
-              Years
-            </label>
-            <input
-              type="number"
-              id="years"
-              name="years"
-              value={personalDetails.age.years}
-              onChange={event =>
-                setPersonalDetails({
-                  ...personalDetails,
-                  age: { ...personalDetails.age, years: event.target.valueAsNumber },
-                })
-              }
-            />
-          </div>
-          <div className={styles.subInput}>
-            <label className={styles.fieldSubLabel} htmlFor="months">
-              Months
-            </label>
-            <input
-              type="number"
-              id="months"
-              name="months"
-              value={personalDetails.age.months}
-              onChange={event =>
-                setPersonalDetails({
-                  ...personalDetails,
-                  age: { ...personalDetails.age, months: event.target.valueAsNumber },
-                })
-              }
-            />
-          </div>
-          <div className={styles.subInput}>
-            <label className={styles.fieldSubLabel} htmlFor="days">
-              Days
-            </label>
-            <input
-              type="number"
-              id="days"
-              name="days"
-              value={personalDetails.age.days}
-              onChange={event =>
-                setPersonalDetails({
-                  ...personalDetails,
-                  age: { ...personalDetails.age, days: event.target.valueAsNumber },
-                })
-              }
-            />
-          </div>
-        </div>
-        <div className={styles.input}>
-          <div className={styles.subInput}>
-            <input
-              type="date"
-              id="birth-date"
-              name="birth-date"
-              value={personalDetails.birthDate !== null ? dayjs(personalDetails.birthDate).format('YYYY-MM-DD') : ''}
-              min={'0000-01-01'}
-              max={'9999-12-31'}
-              onChange={event => setPersonalDetails({ ...personalDetails, birthDate: event.target.valueAsDate })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <label className={styles.fieldLabel} htmlFor="birth-time">
-              Birth Time
-            </label>
-            <input
-              type="time"
-              id="birth-time"
-              name="birth-time"
-              value={personalDetails.birthTime !== null ? dayjs(personalDetails.birthTime).format('HH:mm') : ''}
-              onChange={event => setPersonalDetails({ ...personalDetails, birthTime: event.target.valueAsDate })}
-            />
-          </div>
-          <div className={styles.subInput}>
-            <label className={styles.fieldLabel} htmlFor="estimate">
-              Estimate
-            </label>
-            <input
-              type="checkbox"
-              id="estimate"
-              name="estimate"
-              checked={personalDetails.estimate}
-              onChange={event => setPersonalDetails({ ...personalDetails, estimate: event.target.checked })}
-            />
-          </div>
-        </div>
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Additional Name</h1>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Additional Given Name"
+            value={personalDetails.additionalGivenName}
+            name="additional-given-name"
+            className={styles.fieldInput}
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, additionalGivenName: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Additional Middle Name"
+            value={personalDetails.additionalMiddleName}
+            name="additional-middle-name"
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, additionalMiddleName: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <input
+            type="text"
+            placeholder="Additional Family Name"
+            value={personalDetails.additionalFamilyName}
+            name="additional-family-name"
+            className={styles.fieldInput}
+            disabled={personalDetails.nameUnknown}
+            onChange={event => setPersonalDetails({ ...personalDetails, additionalFamilyName: event.target.value })}
+          />
+        </section>
+      </section>
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Gender</h1>
+        <select
+          name="gender"
+          className={styles.fieldInput}
+          value={personalDetails.gender}
+          onChange={event => setPersonalDetails({ ...personalDetails, gender: event.target.value })}>
+          <option value="" disabled>
+            Select
+          </option>
+          <option value="M">Male</option>
+          <option value="F">Female</option>
+          <option value="O">Other</option>
+          <option value="U">Unknown</option>
+        </select>
+      </section>
+      {personalDetails.estimate ? getAgeInputs() : null}
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Birth Date</h1>
+        <section className={styles.column}>
+          <input
+            type="date"
+            id="birth-date"
+            name="birth-date"
+            className={styles.fieldInput}
+            disabled={personalDetails.estimate}
+            value={personalDetails.birthDate !== null ? dayjs(personalDetails.birthDate).format('YYYY-MM-DD') : ''}
+            min={'0000-01-01'}
+            max={'9999-12-31'}
+            onChange={event => setPersonalDetails({ ...personalDetails, birthDate: event.target.valueAsDate })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Birth Time</h1>
+          <input
+            type="time"
+            id="birth-time"
+            name="birth-time"
+            className={styles.fieldInput}
+            value={personalDetails.birthTime !== null ? dayjs(personalDetails.birthTime).format('HH:mm') : ''}
+            onChange={event => setPersonalDetails({ ...personalDetails, birthTime: event.target.valueAsDate })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Estimate</h1>
+          <input
+            type="checkbox"
+            id="estimate"
+            name="estimate"
+            className={styles.fieldInput}
+            checked={personalDetails.estimate}
+            onChange={event => setPersonalDetails({ ...personalDetails, estimate: event.target.checked })}
+          />
+        </section>
       </section>
     </main>
   );
--- src/patient-registration/section/personal-details.test.tsx
+++ src/patient-registration/section/personal-details.test.tsx
@@ -7,7 +7,7 @@ const mockProps = {
   onChange: jest.fn(),
 };
 
-describe('personal details interaction', () => {
+describe('name interaction', () => {
   let wrapper: ReactWrapper;
 
   beforeEach(() => {
@@ -25,20 +25,6 @@ describe('personal details interaction', () => {
     });
   };
 
-  const runCheckboxInput = (fieldName: string) => {
-    it('updates ' + fieldName, () => {
-      wrapper.find('input[name="' + fieldName + '"]').simulate('change', { target: { checked: true } });
-      expect(wrapper.find('input[name="' + fieldName + '"]').prop('checked')).toEqual(true);
-    });
-  };
-
-  const runNumberInput = (fieldName: string) => {
-    it('updates ' + fieldName, () => {
-      wrapper.find('input[name="' + fieldName + '"]').simulate('change', { target: { valueAsNumber: 1 } });
-      expect(wrapper.find('input[name="' + fieldName + '"]').prop('value')).toEqual(1);
-    });
-  };
-
   runTextInput('given-name');
   runTextInput('middle-name');
   runTextInput('family-name');
@@ -46,29 +32,28 @@ describe('personal details interaction', () => {
   runTextInput('additional-middle-name');
   runTextInput('additional-family-name');
 
-  runCheckboxInput('name-unknown');
-  runCheckboxInput('estimate');
-
-  runNumberInput('years');
-  runNumberInput('months');
-  runNumberInput('days');
-
   it('clears the name fields when the unknown checkbox is checked', () => {
     wrapper.find('input[name="given-name"]').simulate('change', { target: { value: 'Test' } });
     wrapper.find('input[name="name-unknown"]').simulate('change', { target: { checked: true } });
     expect(wrapper.find('input[name="given-name"]').prop('value')).toEqual('');
   });
 
-  it('updates birth-date', () => {
-    let testDate = '2020-05-10';
-    wrapper.find('input[name="birth-date"]').simulate('change', { target: { valueAsDate: testDate } });
-    expect(wrapper.find('input[name="birth-date"]').prop('value')).toEqual(testDate);
+  describe('field validation', () => {
+    it('test passes placeholder', () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
+
+describe('gender interaction', () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<PersonalDetails onChange={mockProps.onChange} />);
   });
 
-  it('updates birth-time', () => {
-    let testTime = dayjs('05:11', 'HH:mm').toDate();
-    wrapper.find('input[name="birth-time"]').simulate('change', { target: { valueAsDate: testTime } });
-    expect(wrapper.find('input[name="birth-time"]').prop('value')).toEqual(dayjs(testTime).format('HH:mm'));
+  afterEach(() => {
+    wrapper = null;
   });
 
   it('updates gender', () => {
@@ -77,8 +62,53 @@ describe('personal details interaction', () => {
   });
 });
 
-describe('field validation', () => {
-  it('test passes placeholder', () => {
-    expect(true).toEqual(true);
+describe('birth date and age interaction', () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<PersonalDetails onChange={mockProps.onChange} />);
+  });
+
+  afterEach(() => {
+    wrapper = null;
+  });
+
+  it('updates birth date', () => {
+    let testDate = '2020-05-10';
+    wrapper.find('input[name="birth-date"]').simulate('change', { target: { valueAsDate: testDate } });
+    expect(wrapper.find('input[name="birth-date"]').prop('value')).toEqual(testDate);
+  });
+
+  it('updates birth time', () => {
+    let testTime = dayjs('05:11', 'HH:mm').toDate();
+    wrapper.find('input[name="birth-time"]').simulate('change', { target: { valueAsDate: testTime } });
+    expect(wrapper.find('input[name="birth-time"]').prop('value')).toEqual(dayjs(testTime).format('HH:mm'));
+  });
+
+  it('shows age inputs on estimate check', () => {
+    wrapper.find('input[name="estimate"]').simulate('change', { target: { checked: true } });
+    expect(wrapper.find('input[name="birth-date"]').prop('disabled')).toEqual(true);
+    expect(wrapper.find('input[name="years"]')).toHaveLength(1);
+    expect(wrapper.find('input[name="months"]')).toHaveLength(1);
+  });
+
+  it('updates birth date', () => {
+    let testYears = 1;
+    let testMonths = 2;
+    wrapper.find('input[name="estimate"]').simulate('change', { target: { checked: true } });
+    wrapper.find('input[name="years"]').simulate('change', { target: { valueAsNumber: testYears } });
+    wrapper.find('input[name="months"]').simulate('change', { target: { valueAsNumber: testMonths } });
+    expect(wrapper.find('input[name="birth-date"]').prop('value')).toEqual(
+      dayjs()
+        .subtract(testYears, 'year')
+        .subtract(testMonths, 'month')
+        .format('YYYY-MM-DD'),
+    );
+  });
+
+  describe('field validation', () => {
+    it('test passes placeholder', () => {
+      expect(true).toEqual(true);
+    });
   });
 });
