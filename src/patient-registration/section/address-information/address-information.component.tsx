515257cc3f30525df5397ec74e3f2c2807a2dd72
Nicholas J Hill <nick.hill@thoughtworks.com>
2020-06-04 14:03:47 +0100

    NH - MF-183 - Patient address general template

--- src/patient-registration/section/address-information/address-information.component.tsx
+++ src/patient-registration/section/address-information/address-information.component.tsx
@@ -1,18 +1,168 @@
 import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 import styles from './address-information.css';
 
 interface AddressInformationProps {
   onChange(addressInformation: AddressInformationState): void;
 }
 
-export interface AddressInformationState {}
+export interface AddressInformationState {
+  address1: string;
+  address2: string;
+  country: string;
+  cityVillage: string;
+  stateProvince: string;
+  postalCode: string;
+  latitude: number;
+  longitude: number;
+  startDate: Date;
+  endDate: Date;
+}
 
 export function AddressInformation(props: AddressInformationProps) {
-  const [addressInformation, setAddressInformation] = useState<AddressInformationState>({});
+  const [addressInformation, setAddressInformation] = useState<AddressInformationState>({
+    address1: '',
+    address2: '',
+    country: '',
+    cityVillage: '',
+    stateProvince: '',
+    postalCode: '',
+    latitude: null,
+    longitude: null,
+    startDate: null,
+    endDate: null,
+  });
+
+  const [test, setTest] = useState<string>('');
 
   useEffect(() => {
     props.onChange(addressInformation);
   }, [addressInformation]);
 
-  return <main className={styles.container}></main>;
+  const textInputField = (name: string, setter: React.Dispatch<React.SetStateAction<string>>) => {
+    return (
+      <input
+        className={styles.fieldInput}
+        type="text"
+        value={name}
+        name={name}
+        onChange={event => setter(event.target.value)}
+      />
+    );
+  };
+
+  return (
+    <main className={styles.container}>
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Address 1</h1>
+        <input
+          className={styles.fieldInput}
+          type="text"
+          value={addressInformation.address1}
+          name="address1"
+          onChange={event => setAddressInformation({ ...addressInformation, address1: event.target.value })}
+        />
+      </section>
+      <section className={styles.row}>
+        <h1 className={styles.fieldHeader}>Address 2</h1>
+        <input
+          className={styles.fieldInput}
+          type="text"
+          value={addressInformation.address2}
+          name="address2"
+          onChange={event => setAddressInformation({ ...addressInformation, address2: event.target.value })}
+        />
+      </section>
+      <section className={styles.row}>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>City/Village</h1>
+          <input
+            className={styles.fieldInput}
+            type="text"
+            value={addressInformation.cityVillage}
+            name="cityVillage"
+            onChange={event => setAddressInformation({ ...addressInformation, cityVillage: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>State/Province</h1>
+          <input
+            className={styles.fieldInput}
+            type="text"
+            value={addressInformation.stateProvince}
+            name="stateProvince"
+            onChange={event => setAddressInformation({ ...addressInformation, stateProvince: event.target.value })}
+          />
+        </section>
+      </section>
+      <section className={styles.row}>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Country</h1>
+          <input
+            className={styles.fieldInput}
+            type="text"
+            value={addressInformation.country}
+            name="country"
+            onChange={event => setAddressInformation({ ...addressInformation, country: event.target.value })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Postal Code</h1>
+          <input
+            className={styles.fieldInput}
+            type="text"
+            value={addressInformation.postalCode}
+            name="postalCode"
+            onChange={event => setAddressInformation({ ...addressInformation, postalCode: event.target.value })}
+          />
+        </section>
+      </section>
+      <section className={styles.row}>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Latitude</h1>
+          <input
+            className={styles.fieldInput}
+            type="number"
+            value={addressInformation.latitude !== null ? addressInformation.latitude : ''}
+            name="latitude"
+            onChange={event => setAddressInformation({ ...addressInformation, latitude: event.target.valueAsNumber })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Longitude</h1>
+          <input
+            className={styles.fieldInput}
+            type="number"
+            value={addressInformation.longitude !== null ? addressInformation.longitude : ''}
+            name="longitude"
+            onChange={event => setAddressInformation({ ...addressInformation, longitude: event.target.valueAsNumber })}
+          />
+        </section>
+      </section>
+      <section className={styles.row}>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>Start Date</h1>
+          <input
+            className={styles.fieldInput}
+            type="date"
+            value={
+              addressInformation.startDate !== null ? dayjs(addressInformation.startDate).format('YYYY-MM-DD') : ''
+            }
+            name="startDate"
+            onChange={event => setAddressInformation({ ...addressInformation, startDate: event.target.valueAsDate })}
+          />
+        </section>
+        <section className={styles.column}>
+          <h1 className={styles.fieldHeader}>End Date</h1>
+          <input
+            className={styles.fieldInput}
+            type="date"
+            value={addressInformation.endDate !== null ? dayjs(addressInformation.endDate).format('YYYY-MM-DD') : ''}
+            name="endDate"
+            onChange={event => setAddressInformation({ ...addressInformation, endDate: event.target.valueAsDate })}
+          />
+        </section>
+      </section>
+    </main>
+  );
 }
--- src/patient-registration/section/address-information/address-information.test.tsx
+++ src/patient-registration/section/address-information/address-information.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { AddressInformation } from './address-information.component';
 
 const mockProps = {
@@ -7,9 +7,50 @@ const mockProps = {
 };
 
 describe('address information', () => {
-  const wrapper = shallow(<AddressInformation onChange={mockProps.onChange} />);
+  let wrapper: ReactWrapper;
 
-  it('renders a container', () => {
-    expect(wrapper.find('main.container')).toHaveLength(1);
+  beforeEach(() => {
+    wrapper = mount(<AddressInformation onChange={mockProps.onChange} />);
   });
+
+  afterEach(() => {
+    wrapper = null;
+  });
+
+  const runInputStringTest = (name: string) => {
+    it('updates the ' + name + ' value', () => {
+      let testString = 'Test';
+      wrapper.find('input[name="' + name + '"]').simulate('change', { target: { value: testString } });
+      expect(wrapper.find('input[name="' + name + '"]').prop('value')).toEqual(testString);
+    });
+  };
+
+  const runInputNumberTest = (name: string) => {
+    it('updates the ' + name + ' value', () => {
+      let testNumber = 1;
+      wrapper.find('input[name="' + name + '"]').simulate('change', { target: { valueAsNumber: testNumber } });
+      expect(wrapper.find('input[name="' + name + '"]').prop('value')).toEqual(testNumber);
+    });
+  };
+
+  const runInputDateTest = (name: string) => {
+    it('updates the ' + name + ' value', () => {
+      let testDate = '2020-04-01';
+      wrapper.find('input[name="' + name + '"]').simulate('change', { target: { valueAsDate: testDate } });
+      expect(wrapper.find('input[name="' + name + '"]').prop('value')).toEqual(testDate);
+    });
+  };
+
+  runInputStringTest('address1');
+  runInputStringTest('address2');
+  runInputStringTest('cityVillage');
+  runInputStringTest('stateProvince');
+  runInputStringTest('country');
+  runInputStringTest('postalCode');
+
+  runInputNumberTest('latitude');
+  runInputNumberTest('longitude');
+
+  runInputDateTest('startDate');
+  runInputDateTest('endDate');
 });
