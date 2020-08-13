2a96f85d6903ec4ec93db97baabd53e5a6749f1e
Larissa <larissa.guenther@thoughtworks.com>
2020-08-13 13:11:20 +0200

    LG - MF-296 - add test if form validation fails

--- src/patient-registration/patient-registration.test.tsx
+++ src/patient-registration/patient-registration.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, wait, fireEvent } from '@testing-library/react';
 import { PatientRegistration } from './patient-registration.component';
-import * as backendController from './patient-registration.resource'
+import * as backendController from './patient-registration.resource';
 
 describe('patient registration', () => {
   it('renders without crashing', () => {
@@ -27,8 +27,8 @@ describe('patient registration sections', () => {
 describe('form submit', () => {
   it('saves the patient', async () => {
     spyOn(backendController, 'savePatient');
-  
-    const { getByText, getByLabelText } = render (<PatientRegistration />);
+
+    const { getByText, getByLabelText } = render(<PatientRegistration />);
     await wait();
 
     const givenNameInput = getByLabelText('Given Name') as HTMLInputElement;
@@ -45,15 +45,38 @@ describe('form submit', () => {
     fireEvent.change(genderSelect, { target: { value: 'Male' } });
     fireEvent.blur(genderSelect);
     await wait();
-   
+
     fireEvent.click(getByText('Register Patient'));
     await wait();
 
-
-
     expect(backendController.savePatient).toHaveBeenCalledTimes(1);
-    expect(backendController.savePatient).toHaveBeenCalledWith(new AbortController(), 
-    {"identifiers": [{"identifier": "", "identifierType": "05a29f94-c0ed-11e2-94be-8c13b969e334", "location": ""}], "person": {"addresses": [{"address1": "", "address2": "", "cityVillage": "", "country": "", "postalCode": "", "stateProvince": ""}], "attributes": [{"attributeType": "14d4f066-15f5-102d-96e4-000c29c2a5d7", "value": ""}], "birthdate": "1993-08-02", "birthdateEstimated": false, "gender": "M", "names": [{"familyName": "Gaihre", "givenName": "Paul", "middleName": "", "preferred": true}]}});
-    
+    expect(backendController.savePatient).toHaveBeenCalledWith(new AbortController(), {
+      identifiers: [{ identifier: '', identifierType: '05a29f94-c0ed-11e2-94be-8c13b969e334', location: '' }],
+      person: {
+        addresses: [{ address1: '', address2: '', cityVillage: '', country: '', postalCode: '', stateProvince: '' }],
+        attributes: [{ attributeType: '14d4f066-15f5-102d-96e4-000c29c2a5d7', value: '' }],
+        birthdate: '1993-08-02',
+        birthdateEstimated: false,
+        gender: 'M',
+        names: [{ familyName: 'Gaihre', givenName: 'Paul', middleName: '', preferred: true }],
+      },
+    });
+  });
+
+  it('should not save the patient if validation fails', async () => {
+    spyOn(backendController, 'savePatient');
+    const { getByText, getByLabelText } = render(<PatientRegistration />);
+    await wait();
+
+    const givenNameInput = getByLabelText('Given Name') as HTMLInputElement;
+
+    fireEvent.change(givenNameInput, { target: { value: '' } });
+    fireEvent.blur(givenNameInput);
+    await wait();
+
+    fireEvent.click(getByText('Register Patient'));
+    await wait();
+
+    expect(backendController.savePatient).not.toHaveBeenCalled();
   });
 });
