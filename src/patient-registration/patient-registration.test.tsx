05a14fd5454acbf94785e2379f0fe5d7ea801486
Mona Fenzl <mona.fenzl@web.de>
2020-09-17 11:51:05 +0200

    SU MF - MF-303 - move mocked address template out of patient registration test

--- src/patient-registration/patient-registration.test.tsx
+++ src/patient-registration/patient-registration.test.tsx
@@ -4,6 +4,7 @@ import { render, wait } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as backendController from './patient-registration.resource';
 import { PatientRegistration, getDeathInfo, initialFormValues } from './patient-registration.component';
+import { getAddressTemplateMock } from '../../__mocks__/openmrs-esm-api.mock';
 
 describe('patient registration', () => {
   it('renders without crashing', () => {
@@ -63,17 +64,7 @@ describe('form submit', () => {
   };
 
   beforeAll(() => {
-    const predefinedAddressTemplate = {
-      data: {
-        results: [
-          {
-            value:
-              '<org.openmrs.layout.address.AddressTemplate>\r\n     <nameMappings class="properties">\r\n       <property name="postalCode" value="Location.postalCode"/>\r\n       <property name="address2" value="Location.address2"/>\r\n       <property name="address1" value="Location.address1"/>\r\n       <property name="country" value="Location.country"/>\r\n       <property name="stateProvince" value="Location.stateProvince"/>\r\n       <property name="cityVillage" value="Location.cityVillage"/>\r\n     </nameMappings>\r\n     <sizeMappings class="properties">\r\n       <property name="postalCode" value="4"/>\r\n       <property name="address1" value="40"/>\r\n       <property name="address2" value="40"/>\r\n       <property name="country" value="10"/>\r\n       <property name="stateProvince" value="10"/>\r\n       <property name="cityVillage" value="10"/>\r\n       <asset name="cityVillage" value="10"/>\r\n     </sizeMappings>\r\n     <lineByLineFormat>\r\n       <string>address1 address2</string>\r\n       <string>cityVillage stateProvince postalCode</string>\r\n       <string>country</string>\r\n     </lineByLineFormat>\r\n     <elementDefaults class="properties">\r\n            <property name="country" value=""/>\r\n     </elementDefaults>\r\n     <elementRegex class="properties">\r\n            <property name="address1" value="[a-zA-Z]+$"/>\r\n     </elementRegex>\r\n     <elementRegexFormats class="properties">\r\n            <property name="address1" value="Countries can only be letters"/>\r\n     </elementRegexFormats>\r\n   </org.openmrs.layout.address.AddressTemplate>',
-          },
-        ],
-      },
-    };
-    spyOn(backendController, 'getAddressTemplate').and.returnValue(Promise.resolve(predefinedAddressTemplate));
+    spyOn(backendController, 'getAddressTemplate').and.returnValue(getAddressTemplateMock());
   });
 
   it('saves the patient', async () => {
