b6f0a9bde350a29de8a8cef27a0b9ae0dce29613
Mona Fenzl <mona.fenzl@web.de>
2020-10-12 22:11:47 +0200

    MF - MF-328 - adjust search input length for mobile screens and localize placeholder text

--- src/patient-registration/section/contact-info/contact-info-section.component.tsx
+++ src/patient-registration/section/contact-info/contact-info-section.component.tsx
@@ -3,12 +3,15 @@ import { AddressInput } from '../../input/custom-input/address/address-input.com
 import styles from './../section.css';
 import { Autocomplete } from '../../input/autocomplete/autocomplete.component';
 import { getAddressHierarchy } from '../../patient-registration.resource';
+import { useTranslation } from 'react-i18next';
 
 interface ContactInfoSectionProps {
   addressTemplate?: string;
 }
 
 export const ContactInfoSection: React.FC<ContactInfoSectionProps> = ({ addressTemplate }) => {
+  const { t } = useTranslation();
+
   return (
     <section className={styles.formSection} aria-label="Contact Info Section">
       <h5 className={`omrs-type-title-5 ${styles.formSectionTitle}`}>Contact Info</h5>
@@ -16,7 +19,7 @@ export const ContactInfoSection: React.FC<ContactInfoSectionProps> = ({ addressT
       <Autocomplete
         name="addressHierarchySearch"
         label="Search Address"
-        placeholder="Type a city, postal code, state or country to search for an address"
+        placeholder={t('addressSearchPlaceHolder', 'Type a City, State or Country')}
         noResultsMessage="no address found, please enter the address manually"
         getSearchResults={getAddressHierarchy}
       />
