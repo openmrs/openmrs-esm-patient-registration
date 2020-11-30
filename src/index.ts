4115a05041bc82e2b4b5690200a07fddc42c9837
Florian Rappl <florian.rappl@smapiot.com>
2020-11-30 11:56:48 +0100

    Updated with config in root module

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import { defineConfigSchema } from '@openmrs/esm-config';
 import { getAsyncLifecycle } from '@openmrs/esm-react-utils';
 import { backendDependencies } from './openmrs-backend-dependencies';
+import { esmPatientRegistrationSchema } from './config-schemas/openmrs-esm-patient-registration-schema';
 
 const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-const options = {
-  featureName: 'Patient Registration',
-  moduleName: '@openmrs/esm-patient-registration-app',
-};
-
 function setupOpenMRS() {
+  const moduleName = '@openmrs/esm-patient-registration-app';
+
+  const options = {
+    featureName: 'Patient Registration',
+    moduleName,
+  };
+
+  defineConfigSchema(moduleName, esmPatientRegistrationSchema);
+
   return {
     lifecycle: getAsyncLifecycle(() => import('./root.component'), options),
     activate: 'patient-registration',
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { defineConfigSchema } from '@openmrs/esm-config';
 import { PatientRegistration } from './patient-registration/patient-registration.component';
-import { esmPatientRegistrationSchema } from './config-schemas/openmrs-esm-patient-registration-schema';
-
-defineConfigSchema('@openmrs/esm-patient-registration-app', esmPatientRegistrationSchema);
 
 export default function Root() {
   return (
