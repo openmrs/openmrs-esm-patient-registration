abd6cb6c03a4d98ac03006f69ab49b3dd611d14c
Florian Rappl <florian.rappl@smapiot.com>
2021-02-04 17:27:28 +0100

    Added breadcrumbs, use async extension

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { registerBreadcrumbs } from '@openmrs/esm-api';
 import { defineConfigSchema } from '@openmrs/esm-config';
-import { getAsyncLifecycle } from '@openmrs/esm-react-utils';
+import { getAsyncExtensionLifecycle, getAsyncLifecycle } from '@openmrs/esm-react-utils';
 import { backendDependencies } from './openmrs-backend-dependencies';
 import { esmPatientRegistrationSchema } from './config-schemas/openmrs-esm-patient-registration-schema';
 
@@ -7,6 +8,7 @@ const importTranslation = require.context('../translations', false, /.json$/, 'l
 
 function setupOpenMRS() {
   const moduleName = '@openmrs/esm-patient-registration-app';
+  const pageName = 'patient-registration';
 
   const options = {
     featureName: 'Patient Registration',
@@ -15,19 +17,27 @@ function setupOpenMRS() {
 
   defineConfigSchema(moduleName, esmPatientRegistrationSchema);
 
+  registerBreadcrumbs([
+    {
+      path: `${window.spaBase}/${pageName}`,
+      title: 'Patient Registration',
+      parent: `${window.spaBase}/home`,
+    },
+  ]);
+
   return {
     lifecycle: getAsyncLifecycle(() => import('./root.component'), options),
-    activate: 'patient-registration',
+    activate: pageName,
     extensions: [
       {
         id: 'registration-home-link',
         slot: 'home-page-buttons',
-        load: getAsyncLifecycle(() => import('./home-link'), options),
+        load: getAsyncExtensionLifecycle(() => import('./home-link'), options),
       },
       {
         id: 'registration-nav-link',
         slot: 'nav-menu',
-        load: getAsyncLifecycle(() => import('./nav-link'), options),
+        load: getAsyncExtensionLifecycle(() => import('./nav-link'), options),
       },
     ],
   };
