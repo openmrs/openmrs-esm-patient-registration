9118bc135072d77fcd783bb2b97cb492820d2f95
nk183 <nk.er13.6@gmail.com>
2021-03-04 21:36:30 +0530

    MF-486 : Added Warning for characters which are not letters in patient names

--- src/patient-registration/validation/patient-registration-validation.tsx
+++ src/patient-registration/validation/patient-registration-validation.tsx
@@ -1,8 +1,19 @@
 import * as Yup from 'yup';
 
 export const validationSchema = Yup.object({
-  givenName: Yup.string().required('Given name is required'),
-  familyName: Yup.string().required('Family name is required'),
+  givenName: Yup.string()
+    .required('Given name is required')
+    .test('alphabets', 'Only letters (A-Z,a-z) are allowed', value => {
+      return /^[A-Za-z]+$/.test(value);
+    }),
+  middleName: Yup.string().test('alphabets', 'Only letters (A-Z,a-z) are allowed', value => {
+    return /^[A-Za-z]+$/.test(value);
+  }),
+  familyName: Yup.string()
+    .required('Family name is required')
+    .test('alphabets', 'Only letters (A-Z,a-z) are allowed', value => {
+      return /^[A-Za-z]+$/.test(value);
+    }),
   additionalGivenName: Yup.string().when('addNameInLocalLanguage', {
     is: true,
     then: Yup.string().required('Given name is required'),
