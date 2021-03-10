38d4d77b1405931cd07cb3c6d4bac732c2054418
Ujjwal Tyagi <ujjwaltyagi355@gmail.com>
2021-03-10 17:32:59 +0530

    Created Links for Extension slot of Add Patient Button on Primary navbar (#74)
    * Created Links for Extension slot of Add Patient Button on Primary navbar
    
    * Trying to remove the build error.
    
    * made the change in the import
    
    * Required changes made
    
    * Made changes according to Prettier Recommendation
    
    * Made changes according to Prettier Recommendation
    
    * Changes to the Add Button import statement

---
+++ src/add-patient-link.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ConfigurableLink } from '@openmrs/esm-framework';
+import Add20 from '@carbon/icons-react/es/add/20';
+import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
+
+export default function Root() {
+  return (
+    <HeaderGlobalAction aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
+      <ConfigurableLink to="${openmrsSpaBase}/patient-registration">
+        <Add20 />
+      </ConfigurableLink>
+    </HeaderGlobalAction>
+  );
+}
