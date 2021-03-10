59f25a9f13d5e3ffea1201662b946bb23700eb36
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-03-10 08:26:23 +0530

    Made changes according to Prettier Recommendation

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import { Add20 } from '@carbon/icons-react';
-import { HeaderGlobalAction } from "carbon-components-react/es/components/UIShell";
+import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
 
 export default function Root() {
   return (
     <HeaderGlobalAction
-      aria-label="Add"
-      aria-labelledby="Add Patient"
-      name="AddPatientIcon"
-    >
+    aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
       <ConfigurableLink to="${openmrsSpaBase}/patient-registration">
         <Add20 />
       </ConfigurableLink>
