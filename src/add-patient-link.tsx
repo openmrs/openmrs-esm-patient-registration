97b6fbafa75199db4eacfa25cc75859662f4d22b
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-03-10 19:49:05 +0530

    Changed the color of add icon from blue to white

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import Add20 from '@carbon/icons-react/es/add/20';
 import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
-
+import './add-patient-link.css'
 export default function Root() {
   return (
     <HeaderGlobalAction aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
       <ConfigurableLink to="${openmrsSpaBase}/patient-registration">
-        <Add20 />
+        <Add20 classname="add-patient-color" />
       </ConfigurableLink>
     </HeaderGlobalAction>
   );
