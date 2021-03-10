023f87ed08f67c5d23d69c882086dca52a4d41ae
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-03-10 20:33:18 +0530

    Made the required changes according to Prettier

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import Add20 from '@carbon/icons-react/es/add/20';
 import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
-import './add-patient-link.css'
+import './add-patient-link.css';
 export default function Root() {
   return (
     <HeaderGlobalAction aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
