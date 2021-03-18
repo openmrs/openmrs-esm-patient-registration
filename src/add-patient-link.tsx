e8734e85c678cd2014c547da609652794aa9de2e
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-03-18 10:44:30 +0530

    Changed the color of Add-Patient Button

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import Add20 from '@carbon/icons-react/es/add/20';
 import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
-import './add-patient-link.css';
 export default function Root() {
   return (
     <HeaderGlobalAction aria-label="Add" aria-labelledby="Add Patient" name="AddPatientIcon">
-      <ConfigurableLink to="${openmrsSpaBase}/patient-registration">
-        <Add20 className="add-patient-color" />
+      <ConfigurableLink style={{ color: '#FFFFFF' }} to="${openmrsSpaBase}/patient-registration">
+        <Add20 />
       </ConfigurableLink>
     </HeaderGlobalAction>
   );
