ed1af87693e80931fabf2ef091871594c0ae3fd5
Florian Rappl <florian.rappl@smapiot.com>
2021-03-25 17:30:51 +0100

    Use flex to position the + better

--- src/add-patient-link.tsx
+++ src/add-patient-link.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ConfigurableLink } from '@openmrs/esm-framework';
 import Add20 from '@carbon/icons-react/es/add/20';
+import { ConfigurableLink } from '@openmrs/esm-framework';
 import { HeaderGlobalAction } from 'carbon-components-react/es/components/UIShell';
 import styles from './add-patient-link.css';
+
 export default function Root() {
   return (
     <HeaderGlobalAction
